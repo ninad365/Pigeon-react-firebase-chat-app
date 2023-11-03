18521628d4883a5db3541210540de9956e276c57
Ninad Ambade <ninad.ambade@aploris.com>
2023-11-03 20:40:57 +0530

    Add users list

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -3,10 +3,10 @@ import { collection, addDoc, orderBy, query, serverTimestamp } from 'firebase/fi
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { auth, db } from '../App.js';
 import ChatMessage from './ChatMessage';
+import UserList from './UserList';
 
 export default function ChatRoom() {
   const dummy = useRef();
-  
   const postConverter = {
     toFirestore(post) {
       return { text: post.text, uid: post.uid, photoURL: post.photoURL, createdAt: post.createdAt };
@@ -51,15 +51,20 @@ export default function ChatRoom() {
   }
 
   return (
-    <>
-      <main>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-        <span ref={dummy}></span>
-      </main>
-      <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-        <button type="submit" disabled={!formValue}>➡️</button>
-      </form>
-    </>
+    <div className="chatroom">
+      <div className="user-list-section">
+        <UserList /> {/* Display the UserList component in the left section */}
+      </div>
+      <div className="chat-messages-section">
+        <main>
+          {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+          <span ref={dummy}></span>
+        </main>
+        <form onSubmit={sendMessage}>
+          <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+          <button type="submit" disabled={!formValue}>➡️</button>
+        </form>
+      </div>
+    </div>
   );
 }
