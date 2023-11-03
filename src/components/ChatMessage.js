fdb4d9178e866295b9c66a21461ec46ce347dfb4
Ninad Ambade <ninad.ambade@aploris.com>
2023-11-03 18:53:16 +0530

    Fix alignment of message and image

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { auth } from '../App.js'
+import { auth } from '../App.js';
 
 export default function ChatMessage(props) {
     const { text, uid, photoURL } = props.message;
   
     const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
   
-    return (<>
-      <div className={`message ${messageClass}`}>
-        <img alt="No img" src={photoURL || 'https://png.pngtree.com/png-vector/20210307/ourlarge/pngtree-simple-capsule-medicine-linear-icon-png-image_3015300.png'} />
-        <p>{text}</p>
-      </div>
-    </>)
-  }
+    return (
+        <div className={`message ${messageClass}`}>
+            <div className="message-content">
+                <img alt="No img" src={photoURL || 'https://png.pngtree.com/png-vector/20210307/ourlarge/pngtree-simple-capsule-medicine-linear-icon-png-image_3015300.png'} />
+                <p>{text}</p>
+            </div>
+        </div>
+    );
+}
