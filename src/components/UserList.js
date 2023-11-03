333cf552d348e7605a8b52bf0bccee56ccfbdfc7
Ninad Ambade <ninad.ambade@aploris.com>
2023-11-03 21:06:34 +0530

    Add users list component

---
+++ src/components/UserList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './UserList.css';
+
+const sampleUsers = [
+    { id: 1, name: 'Global', current: true },
+  { id: 2, name: 'User' },
+  { id: 3, name: 'User2' },
+];
+
+const UserList = () => (
+  <div className="user-list">
+    <h2>Chats</h2>
+    <ul>
+      {sampleUsers.map((user) => (
+        <li key={user.id} className={`user-item ${user.current ? 'current-user' : ''}`}>
+          <div className="user-info">
+            <h3>{user.name}</h3>
+          </div>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default UserList;
