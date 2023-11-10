ed453f6457883acf771391f722e986747baefc53
Ninad Ambade <ninadambade@gmail.com>
2023-11-11 00:34:54 +0530

    Make userlist clickable

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,25 +1,33 @@
+// UserList.js
 import React from 'react';
 import './UserList.css';
 
 const sampleUsers = [
-    { id: 1, name: 'Global', current: true },
+  { id: 1, name: 'Global', current: true },
   { id: 2, name: 'User' },
   { id: 3, name: 'User2' },
 ];
 
-const UserList = () => (
-  <div className="user-list">
-    <h2>Chats</h2>
-    <ul>
-      {sampleUsers.map((user) => (
-        <li key={user.id} className={`user-item ${user.current ? 'current-user' : ''}`}>
-          <div className="user-info">
-            <h3>{user.name}</h3>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const UserList = ({ onUserClick }) => {
+  const handleUserClick = (user) => {
+    // Handle user click, update current user, etc.
+    onUserClick(user);
+  };
+
+  return (
+    <div className="user-list">
+      <h2>Chats</h2>
+      <ul>
+        {sampleUsers.map((user) => (
+          <li key={user.id} className={`user-item ${user.current ? 'current-user' : ''}`}>
+            <div className="user-info" onClick={() => handleUserClick(user)}>
+              <h3>{user.name}</h3>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default UserList;
