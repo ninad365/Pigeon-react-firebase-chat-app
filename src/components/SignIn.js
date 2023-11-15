0ede5e291a21a17e6c2d0100b95ddaad33741e0f
Ninad Ambade <ninadambade@gmail.com>
2023-11-16 03:07:34 +0530

    Save user details in firestore when signing up

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { auth } from '../App.js'
+import { auth, db } from '../App.js'
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, setDoc } from 'firebase/firestore';
 
 export default function SignIn() {
 
     const signInWithGoogle = () => {
         const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider);
+        signInWithPopup(auth, provider)
+        .then(async (result) => {
+            await setDoc(doc(db, "users", result.user.uid), {
+                email: result.user.email, 
+                photoUrl: result.user.photoURL,
+                displayName: result.user.displayName
+            });
+          });
     }
 
     return (
